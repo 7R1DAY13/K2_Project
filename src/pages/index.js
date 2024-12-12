360b6b0e3e60846fb6647209d16d845c9095ae1e
lwinnaingtun <lwinnt72@gmail.com>
2024-12-12 08:19:42 +0630

    combined

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
-import Navbar from "@/pages/component/Navbar";
+import FilterBar from "./component/Filterbar";
+import Listtable from "./component/Listtable";
+import Navbar from "./component/Navbar";
+import SideBar from "./component/Sidebar";
+import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (  
     <>
-    
+    <div class = {styles.container}>
+      <Navbar/>
+      <div class = {styles.main_div}>
+        <SideBar/>
+        <Listtable/>
+        <FilterBar/>
+      </div>
+    </div>
     </>
   );
 }
